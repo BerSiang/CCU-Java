8b911b76f7bdd7d5ef348e11fe4b24e65800fa9b
HanselHu <apologize0616@gmail.com>
2016-03-12 13:08:03 +0800

    作業批改完成!

--- 404530013_1/src/RectangleTester.java
+++ 404530013_1/src/RectangleTester.java
@@ -1,3 +1,13 @@
+﻿/*
+ * [A]102
+ * [TA's advise]
+ * 1.nextDouble()這個method讓你的程式更為謹慎, 可以讓使用者輸入到正確是一個很不錯的作法, 但x-axis跟y-axis是否可以出現負值呢? 另外你寫的正規表示式似乎不允許你輸入浮點數
+ * 試試看用這個正規表示式允許輸入小數點[0-9]+\\.[0-9]+, 這作法很有趣, extra bonus +2.
+ * 2.getArea(),getPerimeter()這兩個method本身結果就已經是double了, 無須額外的強制轉型.
+ * 3.或許等確保輸入值正確後再產生物件可以讓你的做法更簡單也減少function call的cost, 但你的作法是相當謹慎的, 看的出來你C#學的很不錯. 
+ * */
+
+
 /**
  *  RectangleTester
  *
@@ -9,22 +19,21 @@ import Java.Rectangle;
 
 class RectangleTester {
 	public static void main(String[] args) {
-		/*程式進入點*/
+		/* 程式進入點 */
 
-		/**建立 Rectangle 物件*/
+		/** 建立 Rectangle 物件 */
 		Rectangle rect1 = new Rectangle(18, 57, 25, 15);
 		Rectangle rect2 = new Rectangle(47, 60, 12, 0);
 		Rectangle rect3 = new Rectangle();
 
-		/**顯示 rect1、rect2 物件詳細資訊*/
-		System.out.print("\nObject name:rect1\n" + rect1.toString() + "\nArea = " + 
-			rect1.getArea() + "\nPerimeter = " + rect1.getPerimeter() + "\n\n");
-		System.out.println("Object name:rect2\n" + rect2.toString() + "\nArea = " + 
-			(double)rect2.getWidth() * rect2.getHeight() + 
-			"\nPerimeter = " + (double)(rect2.getWidth() + rect2.getHeight()) * 2 + 
-			"\n");
+		/** 顯示 rect1、rect2 物件詳細資訊 */
+		System.out.print("\nObject name:rect1\n" + rect1.toString() + "\nArea = " + rect1.getArea() + "\nPerimeter = "
+				+ rect1.getPerimeter() + "\n\n");
+		System.out.println(
+				"Object name:rect2\n" + rect2.toString() + "\nArea = " + (double) rect2.getWidth() * rect2.getHeight()
+						+ "\nPerimeter = " + (double) (rect2.getWidth() + rect2.getHeight()) * 2 + "\n");
 
-		/**輸入物件 rect3 的 x, y, width, height*/
+		/** 輸入物件 rect3 的 x, y, width, height */
 		System.out.println("Please set rect3 value,");
 		System.out.print("X:");
 		rect3.setX(nextDouble(0));
@@ -34,49 +43,43 @@ class RectangleTester {
 		rect3.setWidth(nextDouble(1));
 		System.out.print("Height:");
 		rect3.setHeight(nextDouble(1));
-		
-		/*顯示 rect3 物件詳細資訊*/
-		System.out.printf("%nObject name:rect3%n%s%nArea = %f%nPerimeter = %f%n", 
-			rect3.toString(), rect3.getArea(), rect3.getPerimeter());
-	}/*主程式結束*/
+
+		/* 顯示 rect3 物件詳細資訊 */
+		System.out.printf("%nObject name:rect3%n%s%nArea = %f%nPerimeter = %f%n", rect3.toString(), rect3.getArea(),
+				rect3.getPerimeter());
+	}/* 主程式結束 */
 
 	/*
-	 *	由於使用者輸入資料的不可預測性
-	 *	建立nextDouble方法來偵測及糾正輸入資料使其符合預期
-	 *	以避免程式不可預期的例外狀況
+	 * 由於使用者輸入資料的不可預測性 建立nextDouble方法來偵測及糾正輸入資料使其符合預期 以避免程式不可預期的例外狀況
 	 */
 
 	/**
-	  *	建立靜態的Scanner物件以接受使者輸入資料
-	  *	在本類別載入時及建立
-	  */
+	 * 建立靜態的Scanner物件以接受使者輸入資料 在本類別載入時及建立
+	 */
 	static Scanner con = new Scanner(System.in);
 
 	/**
-	  *	建立靜態的 nextDouble 方法
-	  *	負責偵測及糾正使用者輸入資料是否符合預期
-	  *	在本類別載入時及建立
-	  *	@param a 預期的輸入資料類型
-	  *			 (a = 0 時接受任何數字, a > 0 時僅接受正數, a < 0 時僅接受負數)
-	  */
+	 * 建立靜態的 nextDouble 方法 負責偵測及糾正使用者輸入資料是否符合預期 在本類別載入時及建立
+	 * 
+	 * @param a
+	 *            預期的輸入資料類型 (a = 0 時接受任何數字, a > 0 時僅接受正數, a < 0 時僅接受負數)
+	 */
 	static double nextDouble(int a) {
 		String input = con.next();
-		if(a == 0) {
-			while(!input.matches("\\d*")) { //規則表示式，當資料不是數字時，請使用者重新輸入
-			System.out.print("Please enter number:");
-			input = con.next();
+		if (a == 0) {
+			while (!input.matches("\\d*")) { // 規則表示式，當資料不是數字時，請使用者重新輸入
+				System.out.print("Please enter number:");
+				input = con.next();
 			}
-		}
-		else if(a > 0) {
-			while(!input.matches("\\d*[^-]")) { //規則表示式，當資料不是正數時，請使用者重新輸入
-			System.out.print("Please enter positive number:");
-			input = con.next();
+		} else if (a > 0) {
+			while (!input.matches("\\d*[^-]")) { // 規則表示式，當資料不是正數時，請使用者重新輸入
+				System.out.print("Please enter positive number:");
+				input = con.next();
 			}
-		}
-		else if(a < 0) {
-			while(!input.matches("\\d*[-]")) { //規則表示式，當資料不是負數時，請使用者重新輸入
-			System.out.print("Please enter negative number:");
-			input = con.next();
+		} else if (a < 0) {
+			while (!input.matches("\\d*[-]")) { // 規則表示式，當資料不是負數時，請使用者重新輸入
+				System.out.print("Please enter negative number:");
+				input = con.next();
 			}
 		}
 		return Double.parseDouble(input); // 通過檢查後，將資料轉成Double型態並回傳
